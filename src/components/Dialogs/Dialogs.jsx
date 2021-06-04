e1b75c75305e8bf0daad53378ec85d4dc7b6f2a2
yuri <yura.itbox@mail.ru>
2021-06-04 11:05:01 +0300

    added Dilogs page Lesson

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-    <div className={s.content}>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>
+          Yuri
+        </div>
+        <div className={s.dialog}>
+          Nastya
+        </div>
+        <div className={s.dialog}>
+          Tamara
+        </div>
+        <div className={s.dialog}>
+          AudiA6C5
+        </div>
+      </div>
+      <div className={s.messagesItems}>
+        <div className={s.message}>Hi I am Yuri</div>
+        <div className={s.message}>Hi I am Nastya</div>
+        <div className={s.message}>Hi I am MyaU</div>
+        <div className={s.message}>Hi I am AudiA6C5</div>
+      </div>
     </div>
   )
 }
