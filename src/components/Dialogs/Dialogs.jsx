56d578be9d7ded42cbc29d3ec43f8d7991fc278b
yuri <yura.itbox@mail.ru>
2021-06-07 10:46:55 +0300

    added DialogItem and Message in file lesson 26

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogsItems/DilogsItem";
+import Message from "./Message/Message";
 
-
-const DialogItem = (props) => {
-  return (
-    <div className={s.dialog}>
-      <NavLink to={`/dialogs/${props.id}`} activeClassName={s.activeLink}>{props.name}</NavLink>
-    </div>
-  )
-};
-
-const Message = (props) => {
-  return (
-    <div className={s.message}>{props.message}</div>
-  )
-}
 
 const Dialogs = (props) => {
   let dialogsData = [
---
+++ src/components/Dialogs/DialogsItems/DilogsItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from '../Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+
+const DialogItem = (props) => {
+  return (
+    <div className={s.dialog}>
+      <NavLink to={`/dialogs/${props.id}`} activeClassName={s.activeLink}>{props.name}</NavLink>
+    </div>
+  )
+};
+
+
+export default DialogItem;
