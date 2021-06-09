b260f3d1896938853902b477857f49f124b30209
yuri <yura.itbox@mail.ru>
2021-06-09 12:06:29 +0300

    aded lesson 38 OOP

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,82 +1,88 @@
-let rerenderEntireTree = () => {
-  console.log('State changed');
-}
+let store = {
+  _state: {
 
-let state = {
+    profilePage: {
 
-  profilePage: {
+      postData: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post!', likesCount: 25},
+        {id: 3, message: 'bla bla bla', likesCount: 3}
+      ],
 
-    postData: [
-      {id: 1, message: 'Hi, how are you?', likesCount: 12},
-      {id: 2, message: 'It\'s my first post!', likesCount: 25},
-      {id: 3, message: 'bla bla bla', likesCount: 3}
-    ],
+      newPostText: 'it-kamasutra.com',
+    },
 
-    newPostText: 'it-kamasutra.com',
+    dialogsPage: {
+      messagesData: [
+        {id: '1', message: 'Hi i am Yuri'},
+        {id: '2', message: 'Hi i am Nastya'},
+        {id: '3', message: 'Hi i am Tamara'},
+        {id: '4', message: 'Hi i am Car'},
+      ],
+
+
+      dialogsData: [
+        {id: '1', name: 'Yuri'},
+        {id: '2', name: 'Nastya'},
+        {id: '3', name: 'Tamara'},
+        {id: '4', name: 'AudiA6C5'}
+      ],
+
+      newMessageTex: 'it-message.com',
+    },
   },
 
-  dialogsPage: {
-    messagesData: [
-      {id: '1', message: 'Hi i am Yuri'},
-      {id: '2', message: 'Hi i am Nastya'},
-      {id: '3', message: 'Hi i am Tamara'},
-      {id: '4', message: 'Hi i am Car'},
-    ],
-
-
-    dialogsData: [
-      {id: '1', name: 'Yuri'},
-      {id: '2', name: 'Nastya'},
-      {id: '3', name: 'Tamara'},
-      {id: '4', name: 'AudiA6C5'}
-    ],
-
-    newMessageTex: 'it-message.com',
+  getState() {
+    return this._state;
   },
+
+  _CallSubscriber() {
+    console.log('State changed');
+  },
+
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 3,
+    };
+    this._state.profilePage.postData.push(newPost);
+    this._state.profilePage.newPostText = '';
+    this._CallSubscriber(this._state);
+    /*мы вызываем эту функцию rerenderEntireTree , но она у нас пустышка . т.к. возвращает
+    консоль лог ....
+     */
+  },
+
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._CallSubscriber(this._state);
+  },
+
+  addMessage() {
+    let newMessage = {
+      id: '5',
+      message: this._state.dialogsPage.newMessageTex,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageTex = '';
+    this._CallSubscriber(this._state);
+  },
+
+  updateNewMessageText(newMessage) {
+    this._state.dialogsPage.newMessageTex = newMessage;
+    this._CallSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._CallSubscriber = observer;
+  },
+
 }
 
-export let addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 3,
-  };
-  state.profilePage.postData.push(newPost);
-  state.profilePage.newPostText = '';
-  rerenderEntireTree(state);
-/*мы вызываем эту функцию rerenderEntireTree , но она у нас пустышка . т.к. возвращает
-консоль лог ....
- */
-};
-
-export let addMessage = () => {
-  let newMessage = {
-    id: '5',
-    message: state.dialogsPage.newMessageTex,
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  state.dialogsPage.newMessageTex = '';
-  rerenderEntireTree(state);
-};
+export default store;
+window.store = store;
 
 
-export let updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export let updateNewMessageText = (newMessage) => {
-  state.dialogsPage.newMessageTex = newMessage;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-/*
-Далее мы создали новую фунцию subscribe = (observer) которая присваевает
-функции  rerenderEntireTree новое значение . и Юзаем её в индекс жс
- */
 
 
-export default state;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state, {subscribe} from "./Redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {addMessage, addPost, updateNewMessageText, updateNewPostText} from "./Redux/state";
+import store from "./Redux/state";
 
 
 let rerenderEntireTree = (state) => {
@@ -15,18 +14,18 @@ let rerenderEntireTree = (state) => {
       <React.StrictMode>
         <App
           state={state}
-          addPost={addPost}
-          addMessage={addMessage}
-          updateNewPostText={updateNewPostText}
-          updateNewMessageText={updateNewMessageText}/>
+          addPost={store.addPost.bind(store)}
+          addMessage={store.addMessage.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
+          updateNewMessageText={store.updateNewMessageText.bind(store)}/>
       </React.StrictMode>
     </BrowserRouter>, document.getElementById('root')
   );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
